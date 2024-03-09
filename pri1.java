4b8b4a81916d736f0839e1ea636c323f13c7057e
gokulgkec <126460522+gokulgkec@users.noreply.github.com>
2024-03-09 14:31:25 +0530

    Add files via upload

---
+++ pri1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class rog
+{
+public static void main(String ar[])
+{
+Scanner s1=new  Scanner(System.in);
+Scanner s2=new  Scanner(System.in);
+System.out.println("Enter two numbers:");
+Int a=s1.nextInt();
+Int b=s2.nextInt();
+Int c=a+b;
+System.out.println(c);
+}
+}
